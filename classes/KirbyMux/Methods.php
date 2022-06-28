191e317e4548d7b4f0bb242197036033c892d55c
dev ofty <63539961+dev-ofty@users.noreply.github.com>
2022-06-28 21:56:09 +0900

    Temporary workaround for audio files
    - No placeholder seems to be available for mux audio?

--- classes/KirbyMux/Methods.php
+++ classes/KirbyMux/Methods.php
@@ -6,8 +6,11 @@ use MuxPhp;
 
 class Methods
 {
-    public static function upload($assetsApi, $url) {
-        $file = $_ENV['MUX_DEV'] === 'true' ? "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4" : $url;
+    public static function upload($assetsApi, $url, $type) {
+        $file = $_ENV['MUX_DEV'] === "true" ? "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4" : $url;
+        if($type === 'audio') :
+        $file = $url;
+        endif;
         $input = new MuxPhp\Models\InputSettings(["url" => $file]);
         $createAssetRequest = new MuxPhp\Models\CreateAssetRequest(["input" => $input, "playback_policy" => [MuxPhp\Models\PlaybackPolicy::_PUBLIC] ]);
         $result = $assetsApi->createAsset($createAssetRequest);
