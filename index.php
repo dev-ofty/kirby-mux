33de06a486dfb10de67f3ef6b22cb3fa5c7188a9
dev ofty <63539961+dev-ofty@users.noreply.github.com>
2022-06-28 21:49:35 +0900

    Add audio

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 @include_once __DIR__ . '/vendor/autoload.php';
 Dotenv\Dotenv::createImmutable(kirby()->root('base'))->load(); // TODO: Add configurable path for .env
 
@@ -16,45 +15,53 @@ Kirby::plugin('robinscholz/kirby-mux', [
     ],
     'blueprints' => [
         'files/mux-video' => __DIR__ . '/blueprints/files/mux-video.yml',
-        'blocks/mux-video' => __DIR__ . '/blueprints/blocks/mux-video.yml'
+        'blocks/mux-video' => __DIR__ . '/blueprints/blocks/mux-video.yml',
+        'files/mux-audio' => __DIR__ . '/blueprints/files/mux-audio.yml',
+        'blocks/mux-audio' => __DIR__ . '/blueprints/blocks/mux-audio.yml'
     ],
     'hooks' => [
         'file.create:after' => function (Kirby\Cms\File $file) {
-            if ($file->type() !== 'video') { return; }
+            if (!in_array($file->type(), ['video','audio'])) {
+                return;
+            }
 
             // Authenticate
             $assetsApi = KirbyMux\Auth::assetsApi();
 
             // Upload the file to mux
-            $result = KirbyMux\Methods::upload($assetsApi, $file->url());
+            $result = KirbyMux\Methods::upload($assetsApi, $file->url(), $file->type());
 
             // Save mux data
             try {
                 $file = $file->update([
-                  'mux' => $result->getData()
+                    'mux' => $result->getData()
                 ]);
-            } catch(Exception $e) {
-                throw new Exception($e->getMessage());
-            }
-        },
-        'file.delete:before' => function (Kirby\Cms\File $file) {
-            if ($file->type() !== 'video') { return; }
-
-            // Authentication setup
-            $assetsApi = KirbyMux\Auth::assetsApi();
-
-            // Get mux Id
-            $muxId = json_decode($file->mux())->id;
-
-            // Delete Asset
-            try {
-                $assetsApi->deleteAsset($muxId);
             } catch (Exception $e) {
                 throw new Exception($e->getMessage());
             }
         },
+        'file.delete:before' => function (Kirby\Cms\File $file) {
+            if (!in_array($file->type(), ['video','audio'])) {
+                return;
+            }
+            if (is_null(json_decode($file->mux())) != 1) :
+                // Authentication setup
+                $assetsApi = KirbyMux\Auth::assetsApi();
+                // Get mux Id
+                $muxId = json_decode($file->mux())->id;
+
+                // Delete Asset
+                try {
+                    $assetsApi->deleteAsset($muxId);
+                } catch (Exception $e) {
+                    throw new Exception($e->getMessage());
+                }
+            endif;
+        },
         'file.replace:before' => function (Kirby\Cms\File $file, Kirby\Filesystem\File $upload) {
-            if ($upload->type() !== 'video') { return; }
+            if (!in_array($upload->type(), ['video','audio'])) {
+                return;
+            }
 
             // Authentication setup
             $assetsApi = KirbyMux\Auth::assetsApi();
@@ -70,7 +77,9 @@ Kirby::plugin('robinscholz/kirby-mux', [
             }
         },
         'file.replace:after' => function (Kirby\Cms\File $newFile, Kirby\Cms\File $oldFile) {
-            if ($newFile->type() !== 'video') { return; }
+            if (!in_array($newFile->type(), ['video','audio'])) {
+                return;
+            }
 
             // Authentication setup
             $assetsApi = KirbyMux\Auth::assetsApi();
@@ -81,10 +90,10 @@ Kirby::plugin('robinscholz/kirby-mux', [
             // Save playback Id
             try {
                 $newFile = $newFile->update([
-                'mux' => $result->getData()
+                    'mux' => $result->getData()
                 ]);
-            } catch(Exception $e) {
-            throw new Exception($e->getMessage());
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage());
             }
         }
     ]
